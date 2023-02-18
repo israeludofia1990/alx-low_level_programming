f7d7b8e7bf84fadff957ea59aa89a8831df6e6a7
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-02-18 11:53:58 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
