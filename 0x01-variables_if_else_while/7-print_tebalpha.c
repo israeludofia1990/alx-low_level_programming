34b842f9e6a0a8b63733a768ac0eb5a7ed9aac8f
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-02-18 13:11:11 -0800

    7-print_tebalpha.c

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low = 'z';
+
+	while (low >= 'a')
+	{
+		putchar(low);
+		low++;
+	}
+	putchar('\n');
+
+	return (0);
+}
