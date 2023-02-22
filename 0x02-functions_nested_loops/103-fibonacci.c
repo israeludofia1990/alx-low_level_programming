5b5df3174b1a81b1f218298449c537c994b28c67
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-02-21 16:15:29 -0800

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+  * main - sum even fibonacci numbers under 4 million.
+  * Return: Nothing.
+  */
+int main(void)
+{
+	unsigned long a = 1;
+	unsigned long b = 2;
+	unsigned long c = 0;
+	unsigned long sum_even;
+	int numb = 0;
+
+	while (numb < 50)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		if (c % 2 == 0 && c < 4000000)
+		{
+			sum_even += c;
+		}
+		numb++;
+	}
+	printf("%lu\n", sum_even);
+}
