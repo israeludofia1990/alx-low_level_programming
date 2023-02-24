ebf77fbd613801bcc95329acdf6da12af1b6e796
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-02-23 18:15:34 -0800

    positive_or_negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,23 +5,16 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - this where my program startes
- *
+ *positive_or_negative - print a number
+ *@i : number to test
  * Return: must always be 0 (success)
  */
-int main(void)
+void positive_or_negative(int i)
 {
-        int n;
-
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-        if (n > 0)
-                printf("%d is positive\n", n);
-        else if (n == 0)
-                printf("%d is zero\n", n);
+        if (i > 0)
+                printf("%d is positive\n", i);
+        else if (i == 0)
+                printf("%d is zero\n", i);
         else
-                printf("%d is negative\n", n);
-
-        return (0);
+                printf("%d is negative\n", i);
 }
