98704d17b2790902849ecafc3c08d37528b45fa2
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-03-15 05:35:02 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *ar;
 
+	if (size <= 0)
+		return (NULL);
 	ar = (char *)malloc(size * sizeof(char));
+	if (ar == 0)
+		return (NULL);
 	while (i < size)
 	{
 		ar[i] = c;
