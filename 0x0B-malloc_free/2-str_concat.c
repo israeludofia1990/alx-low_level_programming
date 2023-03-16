8b6463b6baebdf1e0f61ffd526749c096bbf01fb
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-03-16 11:48:28 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,16 +18,16 @@ char *str_concat(char *s1, char *s2)
 	char *empty = "";
 
 /** if the content of s1 and s2 is NULL treat as empty string */
-	if (*s1 == NULL)
+	if (s1 == NULL)
 		s1 = empty;
-	if (*s2 == NULL)
+	if (s2 == NULL)
 		s2 = empty;
 
 /** get the length of s1 and s2 */
-	s1_length = 1;
+	s1_length = 0;
 	while (s1[s1_length])
 		s1_length++;
-	s2_length = 1;
+	s2_length = 0;
 	while (s2[s2_length])
 		s2_length++;
 	size = s1_length + s2_length + 1;
