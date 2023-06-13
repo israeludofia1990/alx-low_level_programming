dd07d01d64165bea053af0e17fe9a1f00e364370
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-06-13 15:34:28 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	char *new_space;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	len_s1 = _str_len(s1);
 	len_s2 = _str_len(s2);
 
