086c1c678e7035020d611178ebdbc70a2d3a465c
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-06-15 08:06:57 -0700

    main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+/** 0x0B. C - malloc, free */
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+/** 0x0C. C - More malloc, free */
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+#endif
