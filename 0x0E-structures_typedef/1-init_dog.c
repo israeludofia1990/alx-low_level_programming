da91c9b0e73371e007fa7ef3cd5595af998dc5fa
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-06-19 11:38:17 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - initializes members of struct dog
+ * @d: new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
