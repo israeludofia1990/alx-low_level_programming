800a353f9432b331ca9b943912ecd4981fc5d7b3
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-07-03 14:18:13 -0700

    3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+/**
+ * add_node_end - Appends a node
+ * @head: Beginning of list
+ * @str: string to duplicate
+ * Return: newnode
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode;
+	list_t *tmp;
+
+	if (str == NULL)
+		return (NULL);
+
+	newnode = malloc(sizeof(list_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->str = strdup(str);
+	if (newnode->str == NULL)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	newnode->next = NULL;
+
+	if (*head == NULL)
+		*head = newnode;
+	else
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = newnode;
+	}
+	return (newnode);
+}
