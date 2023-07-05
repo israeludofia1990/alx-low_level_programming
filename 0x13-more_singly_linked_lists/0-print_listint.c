02384d3b9ec89c11bf5162715f1e71d095ad376b
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-07-05 11:52:11 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_listint - prints a list
+ * @h: head
+ * Return: count
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		count += 1;
+		h = h->next;
+	}
+	return (count);
+}
