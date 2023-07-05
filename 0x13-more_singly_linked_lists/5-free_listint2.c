fa70cfe815f8c3a877121cce392f83363964e949
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-07-05 13:08:47 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - deletes node
+ * @head: head
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+	listint_t *tmp;
+
+	if (*head == NULL)
+		return;
+	current = *head;
+	while (current != NULL)
+	{
+		tmp = current->next;
+		free(current);
+		current = tmp;
+	}
+	*head = NULL;
+}
