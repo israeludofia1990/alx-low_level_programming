748c98e11fa119106a6975bc9b3a4909c52de576
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-08-17 18:11:17 +0000

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,17 +13,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (newnode == NULL)
 		return (NULL);
-	if (*head == NULL)
-		*head = newnode;
-	else
-	{
+
 		newnode->next = NULL;
 		newnode->prev = NULL;
 		newnode->n = n;
 		newnode->next = *head;
 		(*head)->prev = newnode;
 		*head = newnode;
-	}
 
 	return (newnode);
 }
